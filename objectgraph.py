282d8286b240c637a31d56a3ed3b8fd2c5884f4c
Pete Vilter <bonasaurus1@gmail.com>
2009-03-23 15:08:59 -0400

    Revert "started laying the groundwork for pure object-orientation"
    This reverts commit 0a047ca50ad2679739e41f803bc35d9164544032.

--- objectgraph.py
+++ objectgraph.py
@@ -1,24 +1,16 @@
-latest_id = 0
+object_id = 0
 variables = {}
 objects = {}
 
 def set_var(name, value):
-  global variables, objects
-  new_id = get_next_id()
-  objects[new_id] = value
-  variables[name] = new_id
+  global object_id, variables, objects
+  objects[object_id] = value
+  variables[name] = object_id
+  object_id += 1
 
 def get_var(name):
-  global variables
+  global object_id, variables
   try:
-    return get_object(variables[name])
+    return objects[variables[name]]
   except KeyError:
     raise NameError(name) # TODO: exception handling...
-
-def get_object(object_id):
-  return objects[object_id]
-
-def get_next_id():
-  new_id = latest_id
-  latest_id += 1
-  return new_id
