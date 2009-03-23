0a047ca50ad2679739e41f803bc35d9164544032
Pete <bonasaurus1@gmail.com>
2009-03-22 21:40:07 -0400

    started laying the groundwork for pure object-orientation

--- objectgraph.py
+++ objectgraph.py
@@ -1,16 +1,24 @@
-object_id = 0
+latest_id = 0
 variables = {}
 objects = {}
 
 def set_var(name, value):
-  global object_id, variables, objects
-  objects[object_id] = value
-  variables[name] = object_id
-  object_id += 1
+  global variables, objects
+  new_id = get_next_id()
+  objects[new_id] = value
+  variables[name] = new_id
 
 def get_var(name):
-  global object_id, variables
+  global variables
   try:
-    return objects[variables[name]]
+    return get_object(variables[name])
   except KeyError:
     raise NameError(name) # TODO: exception handling...
+
+def get_object(object_id):
+  return objects[object_id]
+
+def get_next_id():
+  new_id = latest_id
+  latest_id += 1
+  return new_id
