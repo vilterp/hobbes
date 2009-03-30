8d3ea8b5586926cb23e798d34134514ef0875b91
Pete Vilter <bonasaurus1@gmail.com>
2009-03-30 17:12:29 -0400

    started ebnf-based parser

--- parser.py
+++ parser.py
@@ -1,131 +1,80 @@
-import interpreter
+# inspired by http://dealmeida.net/journal/zen-of-parsing
+
+import interpreter, re
 from errors import UnexpectedEOL
 from syntaxtree import Node
 
-def parse(tokens):
-  return Parser(tokens).parse()
+def parse(line):
+  global p
+  try:
+    return p.parse(line)
+  except NameError:
+    p = Parser()
+    parse(line)
 
 class Parser:
   
-  def __init__(self, tokens):
-    self.tokens = tokens
+  def __init__(self, grammarfile='grammar.ebnf'):
     self.pos = 0
     self.stack = []
+    self.rules_list = [tuple(rule.split(' ::= ')) for rule in
+                       open(grammarfile).read().split(' ;\n')
+                       if rule and not rule.startswith('#')]
+    self.rules_dict = {}
+    for name, rule in self.rules_list:
+      self.rules_dict[name] = rule
   
-  def parse(self):
-    if self.line():
-      return self.stack[0]
-    else:
-      raise SyntaxError
+  def parse(self, code):
+    self.code = code
+    for name, rule in self.rules_list:
+      self.match_rule(name, rule)
+    del self.code
+    return self.stack
   
-  def line(self):
-    if not self.statement():
-      if not self.expression():
-        return False
-    return True
+  def match_rule(self, rule_name, rule):
+    for rule_segment in rule.split(' , '):
+      result = self.match_segment(rule_name, rule_segment)
+      if result:
+        self.stack.append(result)
   
-  def statement(self):
-    if not self.name():
-      return False
-    if not self.next_is('symbol','='):
-      return False
-    else:
-      self.read()
-    if not self.expression():
-      return False
-    self.push_tree()
-    return True
+  def match_segment(self, rule_name, rule_segment):
+    remainder = self.code[self.pos:] # part of code remaining to be parsed
+    # literal rule
+    literal_match = re.match(r'^"(.*)"$',rule_segment)
+    if literal_match:
+      match = re.match('(%s)' % re.escape(literal_match.group(1)),remainder)
+      self.pos += match.span()[1]
+      return Token(rule_name,match.group(1))
+    # regex rule
+    regex_match = re.match(r'\? (.*) \?',rule_segment)
+    if regex_match:
+      print '(%s)' % regex_match.group(1)
+      match = re.match('(%s)' % regex_match.group(1),remainder)
+      self.pos += match.span()[1]
+      return Token(rule_name,match.group(1))
+    # or rule ("a | b | c")
+    # FIXME: it thinks these are literal rules
+    options = rule_segment.split(' | ')
+    if len(options):
+      for option in options:
+        try:
+          return match_segment(option)
+        except MatchError:
+          pass
+      raise MatchError('couldn\'t match "%s" in rule %s' % (rule_segment, rule_name))
+    # name of another rule
+    return self.match_rule(rule_name,self.rules_dict[rule_name])
   
-  def expression(self):
-    if not self.term():
-      return False
-    while self.add_op():
-      if not self.factor():
-        return False
-      self.push_tree()
-    return True
+
+class Token:
   
-  def term(self):
-    if not self.factor():
-      return False
-    while self.mult_op():
-      if not self.factor():
-        return False
-      self.push_tree()
-    return True
+  def __init__(self, token_type, value):
+    self.type = token_type
+    self.value = value
   
-  def factor(self):
-    if self.name():
-      return True
-    if self.number():
-      return True
-    if self.next_is('symbol','('):
-      self.advance()
-      if not self.expression():
-        return False
-      if not self.next_is('symbol',')'):
-        return False
-    return False
+  def __repr__(self):
+    return '<Token %s (%s)>' % (self.value, self.type)
   
-  def name(self):
-    if self.next_is('word'):
-      self.read()
-      return True
-    else:
-      return False
-  
-  def number(self):
-    if self.next_is('number'):
-      self.read()
-      return True
-    else:
-      return False
-  
-  def add_op(self):
-    if self.next_is('symbol','+') or self.next_is('symbol','-'):
-      self.read()
-      return True
-    else:
-      return False
-  
-  def mult_op(self):
-    if self.next_is('symbol','*') or self.next_is('symbol','/'):
-      self.read()
-      return True
-    else:
-      return False
-  
-  def next_is(self, token_type, value=None):
-    try:
-      next = self.peek()
-      if value is not None:
-        return next.type == token_type and next.value == value
-      else:
-        return next.type == token_type
-    except UnexpectedEOL:
-      return False
-  
-  def peek(self, ahead=0):
-    try:
-      return self.tokens[self.pos]
-    except IndexError:
-      raise UnexpectedEOL
-  
-  def read(self):
-    token = self.peek()
-    self.advance()
-    self.stack.append(token)
-    return token
-  
-  def advance(self):
-    self.pos += 1
-  
-  def rewind(self):
-    self.pos -= 1
-  
-  def push_tree(self):
-    right = self.stack.pop()
-    value = self.stack.pop()
-    left = self.stack.pop()
-    self.stack.append(Node(value,left,right))
-  
+
+class MatchError(Exception):
+  pass
