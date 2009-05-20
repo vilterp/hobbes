2fe37dd0b3803a619b45d865184fc6c3e7cdddeb
Pete <bonasaurus1@gmail.com>
2009-05-19 21:31:50 -0400

    actually added ForLoopNode.java

---
+++ src/hobbes/ast/ForLoopNode.java
@@ -0,0 +1,26 @@
+package hobbes.ast;
+
+public class ForLoopNode implements StatementNode {
+	
+	private VariableNode indexVar;
+	private VariableNode loopVar;
+	private ExpressionNode collection;
+	private BlockNode block;
+	
+	public ForLoopNode(VariableNode i, VariableNode l, ExpressionNode c, BlockNode b) {
+		indexVar = i;
+		loopVar = l;
+		collection = c;
+		block = b;
+	}
+	
+	public String toString() {
+		return "for("
+				+ (indexVar == null ? "" : indexVar + ": ")
+				+ loopVar + ","
+				+ collection + ","
+				+ block
+				+ ")";
+	}
+	
+}
